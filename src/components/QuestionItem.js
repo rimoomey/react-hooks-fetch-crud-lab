fa51d0c69ff42375234de1d691ed4e40cbadc035
Rimon Moomey <rimon.moomey@gmail.com>
2022-11-28 12:58:50 -0600

    finish patching functionality

--- src/components/QuestionItem.js
+++ src/components/QuestionItem.js
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 
 function QuestionItem({ question, handleClick }) {
   const { id, prompt, answers, correctIndex } = question;
+  const [selectedAnswer, setSelectedAnswer] = useState(correctIndex);
 
   const options = answers.map((answer, index) => (
     <option key={index} value={index}>
@@ -13,13 +14,29 @@ function QuestionItem({ question, handleClick }) {
     handleClick(id);
   }
 
+  function patchEntry(event) {
+    fetch(`http://localhost:4000/questions/${id}`, {
+      method: "PATCH",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        correctIndex: event.target.value,
+      }),
+    })
+      .then((res) => res.json())
+      .then(() => {
+        setSelectedAnswer(event.target.value);
+      });
+  }
+
   return (
     <li>
       <h4>Question {id}</h4>
       <h5>Prompt: {prompt}</h5>
       <label>
         Correct Answer:
-        <select defaultValue={correctIndex}>{options}</select>
+        <select defaultValue={correctIndex} onChange={patchEntry}>
+          {options}
+        </select>
       </label>
       <button onClick={deleteButton}>Delete Question</button>
     </li>
